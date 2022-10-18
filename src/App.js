b7d6c182e17c15bfee539865871caac9175ec446
Airan Telles <airan.telles@bravosul.com.br>
2022-10-18 14:47:54 -0300

    Tips report.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import logo from './img/logo.jpg';
-import Report from './components/Report/Report'
+import LeagueByLiveBotTip from './components/LeagueByLiveBotTip/LeagueByLiveBotTip'
+import LeagueByPreMatchTip from './components/LeagueByPreMatchTip/LeagueByPreMatchTip'
 import './App.css';
 
 function App() {
   return (
     <div>
       <header>
-        <div className="navbar bg-base-100 result-miner-navbar">
+        <div className="navbar bg-base-100 result-miner-navbar text-center">
           <img src={logo} className="result-miner-logo" />
           <label className="font-sans">Relatórios</label>
         </div>
       </header>
-      <body>
-        <Report></Report>
-      </body>
+      <div>
+        <div className="flex">
+          <div className="w-1/2">
+            <h1 className="text-2xl font-bold text-center mt-5">Dicas para over de gols no jogo (Ao vivo)</h1>
+            <div className="artboard phone-1 mt-5">
+              <LeagueByLiveBotTip></LeagueByLiveBotTip>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <h1 className="text-2xl font-bold text-center mt-5">Dicas para ambas marcarem (Pré Jogo)</h1>
+            <div className="artboard phone-1 mt-5">
+              <LeagueByPreMatchTip></LeagueByPreMatchTip>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/LeagueByPreMatchTip/LeagueByPreMatchTip.js
@@ -0,0 +1,58 @@
+import './LeagueByPreMatchTip.css'
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Report extends Component {
+  state = {
+    list: []
+  }
+
+  componentDidMount() {
+    axios.get("/leagues_by_pre_match_bot")
+      .then(res => {
+        const list = res.data;
+        this.setState({ list });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>País</th>
+              <th>Dicas</th>
+              <th>Acertos</th>
+              <th>Percentual</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.list
+              .map((league, index) =>
+                <tr key={index}>
+                  <td>
+                    <img
+                      src={league.league.logo}
+                      style={{maxWidth: "25px"}}
+                      alt='League'
+                    />
+                  </td>
+                  <th>{league.league.external_id}</th>
+                  <td>{league.league.name}</td>
+                  <td>{league.league.country}</td>
+                  <td>{league.total_tips}</td>
+                  <td>{league.success_tips}</td>
+                  <td>{league.percent.toFixed(2)}%</td>
+                </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
